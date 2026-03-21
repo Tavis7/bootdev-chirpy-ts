16ed1df71b9086e4c29dae114b948846de4902c4
tavis <tavis@mostlyirrational.com>
2026-03-21 16:05:48 -0600

    Log non-200 responses

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 import express from "express";
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
 const app = express();
 const PORT = 8080;
 
+app.use(middlewareLogResponses);
 app.use("/app", express.static("./src/app"));
 app.get("/healthz", handlerReadiness);
 
 async function handlerReadiness(req: Request, res: Response): Promise<void> {
     res.set("Content-Type", "text/plain");
     res.send("OK");
+}
+
+function middlewareLogResponses(req: Request, res: Response, next: NextFunction): void {
+    res.on("finish", () => {
+        if (res.statusCode !== 200) {
+            console.log(`[NON-OK] ${req.method} ${req.baseUrl || ""}${req.url} - Status: ${res.statusCode}`);
+        }
+    });
+    next();
 }
 
 app.listen(PORT, () => {
