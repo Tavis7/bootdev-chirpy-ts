d970b426fd5117ed5abb431a9ce53b85ff5b8300
tavis <tavis@mostlyirrational.com>
2026-03-26 17:16:22 -0600

    database

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,18 @@
 type APIConfig = {
     fileserverHits: number,
+    dbURL: string,
 };
 
+function envOrThrow(name: string) {
+    let result = process.env[name];
+    if (result === undefined) {
+        throw new Error(`Environment variable not defined: ${name}`);
+    }
+    return result;
+}
+
+process.loadEnvFile();
 export const config:APIConfig = {
     fileserverHits: 0,
+    dbURL: envOrThrow("DB_URL"),
 };
